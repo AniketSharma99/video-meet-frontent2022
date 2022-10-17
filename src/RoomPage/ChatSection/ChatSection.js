71b87462d0065545fcadfda719a691aa5222e95d
Aniket <aniket.sharma@mastersunion.org>
2022-10-17 13:12:24 +0530

    Chat-added

--- src/RoomPage/ChatSection/ChatSection.js
+++ src/RoomPage/ChatSection/ChatSection.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ChatLabel from "./ChatLabel";
-// import Messages from "./Messages";
-// import NewMessage from "./NewMessage";
+import Messages from "./Messages";
+import NewMessage from "./NewMessage";
 
 const ChatSection = () => {
   return (
     <div className="chat_section_container">
       <ChatLabel />
-      {/* <Messages />
-      <NewMessage /> */}
+      <Messages />
+      <NewMessage />
     </div>
   );
 };
