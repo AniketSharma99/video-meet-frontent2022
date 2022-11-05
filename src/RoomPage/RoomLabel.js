7c46fb6df5405a8e91eaccd467adfab1ccf4f07b
Aniket <aniket.sharma@mastersunion.org>
2022-11-05 12:47:13 +0530

    id-fix

--- src/RoomPage/RoomLabel.js
+++ src/RoomPage/RoomLabel.js
@@ -2,10 +2,10 @@ import React from "react";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const RoomLabel = ({ roomId }) => {
-  const copyID = `https://eclectic-buttercream-298f4c.netlify.app/join-room?id=${roomId}`
+  const copyID = roomId
   return (
     <div className="room_label">
-      <p className="room_label_paragraph">ID: {`https://eclectic-buttercream-298f4c.netlify.app/join-room?id=${roomId}`} 
+      <p className="room_label_paragraph">ID:{roomId} 
       <CopyToClipboard text={copyID}>
         <button>Copy ID</button>
       </CopyToClipboard></p>
@@ -14,3 +14,6 @@ const RoomLabel = ({ roomId }) => {
 };
 
 export default RoomLabel;
+
+// https://eclectic-buttercream-298f4c.netlify.app/join-room?id=${roomId}
+// https://eclectic-buttercream-298f4c.netlify.app/join-room
