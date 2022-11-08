cab19f0fac38413acc26f99c451ddf9f80d27ddb
Aniket <aniket.sharma@mastersunion.org>
2022-11-08 11:25:41 +0530

    MoreBtn-Fix

--- src/RoomPage/RoomPage.js
+++ src/RoomPage/RoomPage.js
@@ -2,7 +2,6 @@ import React, { useEffect,useState } from "react";
 import ChatSection from "./ChatSection/ChatSection";
 import ParticipantsSection from "./ParticipantsSection/ParticipantsSection";
 import VideoSection from "./VideoSection/VideoSection";
-import RoomLabel from "./RoomLabel";
 import { connect } from "react-redux";
 import * as webRTCHandler from "../utils/webRTCHandler";
 import Overlay from "./Overlay";
