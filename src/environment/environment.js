ff9f76250b0df9a50e1a34e68304acf19c6b9a1e
Aniket <aniket.sharma@mastersunion.org>
2022-11-18 14:00:36 +0530

    Set-Live_api

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,10 +1,12 @@
 export const environment ={
 
-BACKEND:"https://video-meeting-backend2022.herokuapp.com/api",
-SERVER : "https://video-meeting-backend2022.herokuapp.com/",
+// BACKEND:"https://video-meeting-backend2022.herokuapp.com/api",
+// SERVER : "https://video-meeting-backend2022.herokuapp.com/",
 
 // BACKEND:"https://localhost:5002/api",
 // SERVER:"https://localhost:5002"
 
+BACKEND:"https://video-meet2022.herokuapp.com/api",
+SERVER:" https://video-meet2022.herokuapp.com/"
 
 }
