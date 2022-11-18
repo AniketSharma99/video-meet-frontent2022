e8e55f5f49b701428c0553bdef3406f42e9ff748
Aniket <aniket.sharma@mastersunion.org>
2022-11-18 17:54:10 +0530

    fix-route

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -7,6 +7,6 @@ export const environment ={
 // SERVER:"http://localhost:5002/"
 
 BACKEND:"https://video-meet2022.herokuapp.com/api",
-SERVER:" https://video-meet2022.herokuapp.com/"
+SERVER:"https://video-meet2022.herokuapp.com/"
 
 }
